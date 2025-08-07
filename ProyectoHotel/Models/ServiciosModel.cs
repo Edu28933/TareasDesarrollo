2a7b5c285595f24143720311c4b16f3a1c2f011b
Astrid Chacon <achaconr1@miumg.edu.gt>
2025-08-06 21:48:58 -0600

    ServiciosModel datos

--- ProyectoHotel/Models/ServiciosModel.cs
+++ ProyectoHotel/Models/ServiciosModel.cs
@@ -2,5 +2,11 @@
 {
     public class ServiciosModel
     {
+        public int IdServicio { get; set; }
+        public string? Descripcion { get; set; }
+        public string? TipoServicio { get; set; }
+        public double Precio { get; set; }
+        public string? Estado { get; set; }
+
     }
 }
